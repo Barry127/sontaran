825d32e9bd93a62585ee187561fc7587de29f5df
Barry de Kleijn <kleijn.barry@gmail.com>
2017-01-28 20:05:11 +0100

    Export Validators

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-const Validator = require('./validator/Validator');
+const Validator         = require('./validator/Validator');
+
+const ArrayValidator    = require('./validator/types/Array');
+const BooleanValidator  = require('./validator/types/Boolean');
+const EmailValidator    = require('./validator/types/Email');
+const NetworkValidator  = require('./validator/types/Network');
+const NumberValidator   = require('./validator/types/Number');
+const ObjectValidator   = require('./validator/types/Object');
+const RegExpValidator   = require('./validator/types/RegExp');
+const StringValidator   = require('./validator/types/String');
 
 const validate = function validate (value) {
   return new Validator(value);
 };
 
 module.exports = {
-  validate
+  validate,
+  validators: {
+    ArrayValidator,
+    BooleanValidator,
+    EmailValidator,
+    NetworkValidator,
+    NumberValidator,
+    ObjectValidator,
+    RegExpValidator,
+    StringValidator
+  }
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,15 @@
 const { expect }    = require('chai');
-const { validate }  = require('./index');
+const { validate, validators }  = require('./index');
 const Validator     = require('./validator/Validator');
+
+const ArrayValidator    = require('./validator/types/Array');
+const BooleanValidator  = require('./validator/types/Boolean');
+const EmailValidator    = require('./validator/types/Email');
+const NetworkValidator  = require('./validator/types/Network');
+const NumberValidator   = require('./validator/types/Number');
+const ObjectValidator   = require('./validator/types/Object');
+const RegExpValidator   = require('./validator/types/RegExp');
+const StringValidator   = require('./validator/types/String');
 
 describe('index', () => {
 
@@ -14,6 +23,42 @@ describe('index', () => {
     it('passes the value to Validator instance', () => {
       expect(result.value).to.equal('this');
     });
+  });
+
+  describe('validators', () => {
+
+    it('Exports ArrayValidator', () => {
+      expect(validators.ArrayValidator).to.equal(ArrayValidator);
+    });
+
+    it('Exports BooleanValidator', () => {
+      expect(validators.BooleanValidator).to.equal(BooleanValidator);
+    });
+
+    it('Exports EmailValidator', () => {
+      expect(validators.EmailValidator).to.equal(EmailValidator);
+    });
+
+    it('Exports NetworkValidator', () => {
+      expect(validators.NetworkValidator).to.equal(NetworkValidator);
+    });
+
+    it('Exports NumberValidator', () => {
+      expect(validators.NumberValidator).to.equal(NumberValidator);
+    });
+
+    it('Exports ObjectValidator', () => {
+      expect(validators.ObjectValidator).to.equal(ObjectValidator);
+    });
+
+    it('Exports RegExpValidator', () => {
+      expect(validators.RegExpValidator).to.equal(RegExpValidator);
+    });
+
+    it('Exports StringValidator', () => {
+      expect(validators.StringValidator).to.equal(StringValidator);
+    });
+
   });
 
 });
