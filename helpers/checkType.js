25030f94f8dc8225087bc26432c4a19e27d145a5
Barry de Kleijn <kleijn.barry@gmail.com>
2020-09-04 22:30:09 +0200

    remove helpers

--- helpers/checkType.js
+++
@@ -1,9 +0,0 @@
-function checkType (value, type) {
-  if (typeof type !== 'string') {
-    throw new TypeError('checkType: type argument must be of type string');
-  }
-
-  return (typeof value === type.toLowerCase()); // eslint-disable-line valid-typeof
-}
-
-module.exports = checkType;
